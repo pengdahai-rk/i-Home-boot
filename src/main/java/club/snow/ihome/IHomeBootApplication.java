4375702e3fc5298781f775272f92ce8fd938c276
pengdahai216@126.com <pengdahai216@126.com>
2024-04-17 00:11:50 +0800

    project init

---
+++ src/main/java/club/snow/ihome/IHomeBootApplication.java
@@ -0,0 +1,13 @@
+package club.snow.ihome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHomeBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IHomeBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/snow/ihome/IHomeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package club.snow.ihome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHomeBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
