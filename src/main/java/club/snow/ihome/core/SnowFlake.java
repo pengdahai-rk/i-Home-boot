a447e211166269a6ebebe8f39db7f11e67105b3b
pengdahai-rk <pengdahai216@126.com>
2024-04-19 22:46:50 +0800

    增加雪花算法类

---
+++ src/main/java/club/snow/ihome/core/SnowFlake.java
@@ -0,0 +1,95 @@
+package club.snow.ihome.core;
+
+/**
+ * The type Snow flake.
+ *
+ * @author <a href="mailto:pengdahai216@126.com">pengdahai</a>
+ * @since 2024 /04/18
+ */
+public class SnowFlake {
+
+    /**
+     * 起始的时间戳
+     */
+    private final static long START_STAMP = 1704038400000L;
+
+    /**
+     * 每一部分占用的位数
+     */
+    private final static long SEQUENCE_BIT = 12L; // 序列号占用的位数
+    private final static long MACHINE_BIT = 5L;   // 机器标识占用的位数
+    private final static long DATACENTER_BIT = 5L;// 数据中心占用的位数
+
+    /**
+     * 每一部分的最大值
+     */
+    private final static long MAX_DATACENTER_NUM = ~(-1L << DATACENTER_BIT);
+    private final static long MAX_MACHINE_NUM = ~(-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = ~(-1L << SEQUENCE_BIT);
+
+    /**
+     * 每一部分向左的位移
+     */
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTAMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    private final long datacenterId;  // 数据中心
+    private final long machineId;     // 机器标识
+    private long sequence = 0L; // 序列号
+    private long lastStamp = -1L;// 上一次时间戳
+
+    public SnowFlake(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * Next id long.
+     *
+     * @return the long
+     */
+    public synchronized long nextId() {
+        long currStamp = getNewStamp();
+        if (currStamp < lastStamp) {
+            throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStamp == lastStamp) {
+            // 相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            // 同一毫秒的序列数已经达到最大
+            if (sequence == 0L) {
+                currStamp = getNextMill();
+            }
+        } else {
+            // 不同毫秒内，序列号置为0
+            sequence = 0L;
+        }
+
+        lastStamp = currStamp;
+
+        return (currStamp - START_STAMP) << TIMESTAMP_LEFT // 时间戳部分
+                | datacenterId << DATACENTER_LEFT       // 数据中心部分
+                | machineId << MACHINE_LEFT             // 机器标识部分
+                | sequence;                             // 序列号部分
+    }
+
+    private long getNextMill() {
+        long mill = getNewStamp();
+        while (mill <= lastStamp) {
+            mill = getNewStamp();
+        }
+        return mill;
+    }
+
+    private long getNewStamp() {
+        return System.currentTimeMillis();
+    }
+}
